2751ecc135fabc0b3dfa0557bd2f273823043403
Arun-Maddy <maddy92.u2@gmail.com>
2023-11-23 14:54:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 
 # adding imports
 import streamlit
+import pandas
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
@@ -11,3 +12,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
