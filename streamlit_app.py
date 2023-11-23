1c44c4d22fa5f0c5756b82fb6b5201fd36064007
Arun-Maddy <maddy92.u2@gmail.com>
2023-11-23 14:52:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗  Kale, Spinach & Rocket Smoothie')
